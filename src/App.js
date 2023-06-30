029ab3684531e437d7fec5ede35600cbfea3823a
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2023-06-30 10:06:24 +0530

    Update App.js
    changed the title of the app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Timer from "./Timer";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Hello I am Time</h1>
+      <h2>I dont wait for any one</h2>
       <Timer />
     </div>
   );
